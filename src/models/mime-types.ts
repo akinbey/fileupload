077aa582323263dc986305cad933ac7f050ee503
maxjackal <74346691+maxjackal@users.noreply.github.com>
2024-01-05 11:44:17 +0300

    Update mime-types.ts
    add mime types

--- src/models/mime-types.ts
+++ src/models/mime-types.ts
@@ -1,7 +1,11 @@
 export const MIME_TYPE_MAP: { [key: string]: string } = {
     'image/jpeg': 'jpg',
+    'image/jpeg': 'jpeg',
     'image/png': 'png',
     'application/pdf': 'pdf',
+    'application/zip': 'zip',
+    'application/msword':'doc',
+  
     // Diğer MIME türleri ekleyebilirsiniz.
   };
   
